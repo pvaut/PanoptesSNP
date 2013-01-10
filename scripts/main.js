b3e940611ceb8c168305afc87567956bc3cfd435
Paul Vauterin <pvaut@gmail.com>
2013-01-10 09:46:38 +0000

    Initial commit of boilerplate

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+﻿
+setupRequireJS();
+
+require([DQXSCJQ(), DQXSC("Framework"), DQXSC("Msg"), DQXSC("HistoryManager"), DQXSC("Utils"), "Page"],
+    function ($, Framework, Msg, HistoryManager, DQX, thePage) {
+        $(function () {
+
+            //Global initialisation of utilities
+            DQX.Init();
+
+            setTimeout(function () {
+                //Create the frames
+                thePage.createFramework();
+
+                //Render frames
+                Framework.render(thePage.frameRoot, 'Div1');
+
+                //Some generic stuff after creation of the html
+                DQX.initPostCreate();
+
+                //trigger the initial synchronisation
+                HistoryManager.init();
+            }, 10);
+
+        });
+    });
---
+++ scripts/page.js
@@ -0,0 +1,22 @@
+﻿define(["DQX/Framework", "DQX/HistoryManager", "DQX/DocEl", "DQX/Msg"],
+    function (Framework, HistoryManager, DocEl, Msg) {
+        thePage = {
+
+            createFramework: function () {
+
+                thePage.frameRoot = Framework.FrameGroupVert('');
+                thePage.frameRoot.setMargins(0);
+
+                //The top line of the page
+                thePage.frameHeaderIntro = thePage.frameRoot.addMemberFrame(Framework.FrameFinal('HeaderIntro', 1))
+                    .setFixedSize(Framework.dimY, 75).setFrameClassClient('DQXPage');
+
+                //The body panel of the page
+                thePage.frameBody = thePage.frameRoot.addMemberFrame(Framework.FrameGroupStack('info', 1));
+            },
+
+
+        };
+
+        return thePage;
+    });
